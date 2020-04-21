379511244f706c0676671590f224846e2831ef4c
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-04-21 10:34:55 +0200

    update

--- streamingapp/src/components/streams/Create.js
+++ streamingapp/src/components/streams/Create.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import { Form, Button, Jumbotron } from 'react-bootstrap';
+
 class Create extends Component {
-renderInput(input){
-    
-    
-       return (
-    
-       <div>
-    <input type='text'  {...input}      />
-        </div>
-       )
+    renderInput({ input, label }) {
+
+
+        return (
+
+                <Form.Group>
+                    <Form.Label>{label}</Form.Label>
+                    <Form.Control placeholder="...."{...input}/>
+
+                </Form.Group>
+
+        )
     }
 
+    onSubmit(formValues){
+      
+        console.log(formValues, 'form value / on submit ');
+    }
 
     render() {
 
@@ -20,14 +29,19 @@ renderInput(input){
 
             <div>
 
-                <h3> create page </h3>
+                <h3 style={{paddingTop:'5%', paddingBottom:'5%', textAlign:"center"}}> Create your Strean</h3>
+            <hr></hr>
+            <Jumbotron style={{backgroundColor:"lightGrey", opacity:'0.8'}} >
+                <form style={{opacity:'0.9'}}  onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                    <Field name="title" component={this.renderInput} label="Enter Title" />
+                    <Field name="description" component={this.renderInput} label="Enter Description" />
 
-                <form>
-                    <Field name ="title" component={this.renderInput}/>
-                    <Field name ="description" component={this.renderInput}/>
-                    
+              
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
                 </form>
-
+                </Jumbotron>
 
             </div>
 
