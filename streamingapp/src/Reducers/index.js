fb0ad3fdd827e8c2459297c591d78d7c002729e8
Ash Cole <45434172+ash-sp-cole@users.noreply.github.com>
2020-06-11 11:35:56 +0200

    Update index.js

--- streamingapp/src/Reducers/index.js
+++ streamingapp/src/Reducers/index.js
@@ -2,7 +2,9 @@ import { combineReducers} from 'redux';
 import { authReducer } from './authReduce';
 import {reducer as formReducer} from 'redux-form';
 
+
 export default combineReducers({
    auth: authReducer,
    form: formReducer
+   
 })
